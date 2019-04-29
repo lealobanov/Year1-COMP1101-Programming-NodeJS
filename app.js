2e53407b426fdad7d83a22e3a3c907bdd04499dc
lealobanov <44328396+lealobanov@users.noreply.github.com>
2019-04-30 00:12:20 +0100

    Completed functionality updates

--- app.js
+++ app.js
@@ -13,11 +13,16 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 
-
 //Set up entities
-let users = [{"fname":"Bob", "lname":"Smith","username":"testuser123", "bio": "this is my bio"}];
+let users = [{"fname":"John", "lname":"Doe","username":"john.doe@gmail.com", "bio": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lectus vestibulum mattis ullamcorper velit sed ullamcorper morbi tincidunt."},
+{"fname":"Jane", "lname":"Smith","username":"jane.smith@gmail.com", "bio": "Pellentesque vitae ante vehicula, blandit mauris a, maximus nulla. Donec non enim at velit hendrerit pretium. Nulla iaculis, nunc sit amet volutpat pharetra, erat justo tempor orci, a vulputate tellus turpis sit amet mauris."},
+{"fname":"Tom", "lname":"Clark","username":"tom.clark@gmail.com", "bio": "Integer nibh justo, venenatis at nulla ac, malesuada dignissim dolor. Integer ac vulputate enim. Mauris ac mi a enim consequat maximus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos."}];
+let current_user = [];
 
-let posts = []
+let my_posts = [];
+let posts = [{"posttitle":"Sample Post 1", "postauthor":"john.doe@gmail.com","postdate":"2019-04-12", "postcontent": "Integer accumsan nunc quis lectus maximus, congue cursus dolor porta. Duis ultrices sapien non elit cursus, vitae vestibulum massa fermentum. Aliquam sed ligula viverra, imperdiet dolor ut, ornare lorem. Phasellus erat neque, viverra non ipsum at, mollis porta mi. Vivamus nulla turpis, rutrum non laoreet ut, molestie sed mi. Suspendisse nec pharetra tellus. Nam vitae nibh non nisi rutrum rhoncus. Aliquam ligula nulla, placerat ut lobortis vel, aliquam eu lectus. Nulla eget suscipit eros. Vivamus dignissim ornare vehicula. Nunc eleifend arcu est, vitae fringilla risus sodales ac. Nulla porta vestibulum elementum. Sed non porttitor massa, id ultrices elit. Donec gravida turpis eget laoreet aliquam."},
+{"posttitle":"Sample Post 2", "postauthor":"jane.smith@gmail.com","postdate":"2019-04-20", "postcontent": "Nulla libero urna, condimentum eget magna sit amet, tempor suscipit enim. Ut consectetur interdum sem, vel pretium risus feugiat eu. Sed in sollicitudin nunc. Aliquam ac scelerisque dui. Quisque consequat congue orci, ac feugiat tellus pulvinar vitae. Maecenas vehicula volutpat diam elementum porttitor. Donec in ornare nunc. Aenean scelerisque est quis mauris vulputate, ut sagittis metus lacinia. Pellentesque ultrices leo ipsum, efficitur ultrices sapien molestie in. Integer mollis porta fermentum. Mauris iaculis risus ac scelerisque viverra."},
+{"posttitle":"Sample Post 3", "postauthor":"tom.clark@gmail.com","postdate":"2019-04-24", "postcontent": "Sed aliquam neque est, vitae consectetur sapien sagittis ac. Sed vitae ornare velit. Aenean ut odio vitae quam tempus aliquet id ultricies nulla. Interdum et malesuada fames ac ante ipsum primis in faucibus. Maecenas tristique, turpis sed tempus aliquet, diam quam vestibulum dolor, nec varius purus sapien et lorem. Sed at euismod erat. Curabitur scelerisque libero quis condimentum tristique. Praesent arcu massa, blandit vel rutrum vitae, porta ac dui. Cras placerat condimentum urna, ut ornare lorem. Suspendisse volutpat eget lorem non pretium. Maecenas varius eu nisi vel auctor. Vestibulum vitae purus nec sem faucibus fringilla. Aliquam lobortis dolor nec hendrerit egestas. Etiam ut tincidunt dolor, nec tincidunt metus."}];
 
 
 //Routes
@@ -25,7 +30,7 @@ app.get('/',function(req,res){
   res.sendFile(path.join(__dirname+'/client/index.html'));
 });
 
-//
+//Collect user data upon registration (user authentication handled by Google Firebase Auth API)
 app.post('/newuser', function(req,res){
 	
   if(checkUserExists(req.body.username) !== false){
@@ -47,15 +52,57 @@ app.post('/newuser', function(req,res){
 			bio: req.body.bio
 		};
 
-		passwords.push(req.body.password);
 		users.push(user);
 	
 		res.send(true);
 		console.log('Added new user');
 		
+		
 	}
-	console.log("Request sent: " +req.body);
-})
+	console.log("Request to add new user data sent: " +req.body);
+});
+
+//Post user profile data upon login
+app.post('/login', function(req,res){
+	
+  if(checkUserExists(req.body.username) !== false){
+		console.log('This user has already been stored in the list of current users.')
+		current_user[0] = req.body.username
+	}
+	else{
+		current_user[0] = req.body.username
+		let user = {
+			fname: 'Null',
+      lname: 'Null',	
+			username: req.body.username,
+			bio: 'This user was registered and authenticated by Firebase during a previous server session. To update your profile information, please login as a recurring user.'
+		};
+		users.push(user);
+		console.log('This user was registered and authenticated by Firebase in a previous session.')
+	}
+});
+
+//Post user profile data if logging in from previous server session
+app.post('/loginupd', function(req,res){
+	
+  if(checkUserExists(req.body.username) !== false){
+		console.log('This user has already been stored in the list of current users.')
+		current_user[0] = req.body.username
+	}
+	else{
+		current_user[0] = req.body.username
+		let user = {
+			fname: req.body.fname,
+      lname: req.body.lname,	
+			username: req.body.username,
+			bio: req.body.bio
+		};
+		users.push(user);
+		console.log('Your profile data has been updated.')
+	}
+});
+
+// Functions for searching among user properties
 
 function checkUserExists(username){
 	for (var i = 0; i < users.length; i++) {
@@ -67,41 +114,29 @@ function checkUserExists(username){
 	return false
 };
 
-// Search individual users
-app.get("/users/:username", function(req,res){
-	let index = checkUserExists(req.params.username);
-	if(index !== false){
-		res.send(users[index]);
-	}else{
-		res.send("User not found.");
-	}
+function checkFnameUserExists(fname){
+	for (var i = 0; i < users.length; i++) {
 
-});
+		if (users[i].fname == fname){
+			return i;
+		}
+	}
+	return false
+};
+function checkLnameUserExists(lname){
+	for (var i = 0; i < users.length; i++) {
+
+		if (users[i].lname == lname){
+			return i;
+		}
+	}
+	return false
+};
 
 // List all current users in user directory
 app.get("/users/", function(req,res) {
 	res.send(users)
 });
-
-// Login to website; assigns access token to user. To initiate further post requests (create post, add comment), access token is required.
-app.post("/login",function(req,resp){
-	let existingUser = checkUserExists(req.body.username);
-	if(!req.body.username || !req.body.password || existingUser === false || passwords[existingUser] != req.body.password ){
-		resp.send(false);
-	}else{
-		req.session.isLoggedin = true;
-		req.session.isAdmin= users[existingUser].adminStatus;
-		req.session.username = users[existingUser].username;
-		req.session.save(function(err){});
-		resp.send(true);
-	}
-})
-
-// Logout of site, revert back to initial homescreen
-app.get("/logout",function(req,resp){
-	req.session.destroy();
-	resp.send(true);
-})
 
 // Create a new forum post
 app.post("/createpost",function(req,resp){
@@ -112,12 +147,13 @@ app.post("/createpost",function(req,resp){
 	}else{
 		let post= {
 			posttitle: req.body.posttitle,
-			postauthor: 'assign post author dynamically',
+			postauthor: current_user[0],
 			postdate: req.body.postdate,
 			postcontent: req.body.postcontent,
 		};
 		
 		posts.push(post);
+		my_posts.push(post);
 		resp.send(true);
 	}
 })
@@ -127,24 +163,92 @@ app.get("/posts/", function(req,res) {
 	res.send(posts)
 });
 
-// Comment on an existing post
-app.post("/addcomment",function(req,resp){
+// List all of the logged-in user's posts
+app.get("/myposts/", function(req,res) {
+	res.send(my_posts)
+});
 
-	if(!req.body.eventn || !req.body.comment){
-		
-		resp.send(false);
-	}else{
-		let comment= {
-			username: req.session.username,
-			event: req.body.eventn,
-			comment: req.body.comment,
-			image: req.session.image
-		};
-		
-		comments["comments"].push(comment);
-		resp.send(true);
+// Functions for searchign among post properties
+
+function checkPostExists(posttitle){
+	for (var i = 0; i < posts.length; i++) {
+
+		if (posts[i].posttitle == posttitle){
+			return i;
+		}
 	}
+	return false
+};
+
+function checkPostAuthorExists(postauthor){
+	for (var i = 0; i < posts.length; i++) {
+
+		if (posts[i].postauthor == postauthor){
+			return i;
+		}
+	}
+	return false
+};
+// Search in user library by username (user's email address)
+
+app.get("/users/:username", function(req,res){
+	let index = checkUserExists(req.params.username);
+	if(index !== false){
+		res.send(users[index]);
+	}else{
+		res.send("No user found with this email address.");
+	}
+
 })
+
+// Search in user library by first name
+
+app.get("/users/fname/:fname", function(req,res){
+	let index = checkFnameUserExists(req.params.fname);
+	if(index !== false){
+		res.send(users[index]);
+	}else{
+		res.send("No user found with this first name.");
+	}
+
+})
+
+// Search in user library by last name
+
+app.get("/users/lname/:lname", function(req,res){
+	let index = checkLnameUserExists(req.params.lname);
+	if(index !== false){
+		res.send(users[index]);
+	}else{
+		res.send("No user found with this last name.");
+	}
+
+})
+
+// Search in post library by post title
+
+app.get("/posts/:posttitle", function(req,res){
+	let index = checkPostExists(req.params.posttitle);
+	if(index !== false){
+		res.send(posts[index]);
+	}else{
+		res.send("No post found with this title.");
+	}
+
+})
+
+// Search in post library by post author (user's email address)
+
+app.get("/posts/author/:postauthor", function(req,res){
+	let index = checkPostAuthorExists(req.params.postauthor);
+	if(index !== false){
+		res.send(posts[index]);
+	}else{
+		res.send("No post found with this author.");
+	}
+
+})
+
 //Run server
 app.listen(port,function(){
   console.log(`Server listening on port ${port}`);
